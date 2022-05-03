32186c9d15b4f420dff794c2a0e0256b6beb55eb
lyj00912 <3122742760@qq.com>
2022-05-04 00:42:00 +0800

    removed the GUI part

--- LCD_Show.ino
+++ LCD_Show.ino
@@ -1,41 +1,40 @@
- #include "DEV_Config.h"
+#include "DEV_Config.h"
 #include "LCD_Driver.h"
-#include "LCD_GUI.h"
+//#include "LCD_GUI.h"
+
+
+#define LCD_BACKGROUND    WHITE   //Default background color
+#define FONT_BACKGROUND   WHITE   //Default font background color
+#define FONT_FOREGROUND     GRED    //Default font foreground color
+
+#define WHITE          0xFFFF
+#define BLACK          0x0000
+#define BLUE           0x001F
+#define BRED           0XF81F
+#define GRED       0XFFE0
+#define GBLUE      0X07FF
+#define RED            0xF800
+#define MAGENTA        0xF81F
+#define GREEN          0x07E0
+#define CYAN           0x7FFF
+#define YELLOW         0xFFE0
+#define BROWN        0XBC40
+#define BRRED        0XFC07
+#define GRAY         0X8430
 
 void setup()
 {
   System_Init();
 
-  Serial.println("3.5inch　TFT　Touch Shiled LCD Show...");  
-  Serial.println("LCD Init...");
   LCD_SCAN_DIR Lcd_ScanDir = SCAN_DIR_DFT;  
   LCD_Init( Lcd_ScanDir, 200);
 
-  Serial.println("LCD_Clear...");
   LCD_Clear(LCD_BACKGROUND);
 
-  Serial.println("LCD_Show...");
-  GUI_Show();
+//  GUI_Show();
 }
 
 void loop()
 {
-  DEV_TIME sDev_time;
-  sDev_time.Hour = 23;
-  sDev_time.Min = 38;
-  sDev_time.Sec = 56;
-
-  Serial.print("Show time\r\n");
-  for (;;) {
-    sDev_time.Sec++;
-    GUI_Showtime(200, 150, 327, 197, &sDev_time, RED);
-    Driver_Delay_ms(1000);//Analog clock 1s
-    if (sDev_time.Sec == 60)
-      sDev_time.Sec = 0;
-  }
+  ;
 }
-
-/*********************************************************************************************************
-  END FILE
-*********************************************************************************************************/
-
