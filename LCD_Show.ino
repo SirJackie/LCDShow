f2327ecbd8b2932a0a1e6d0f7ede6ca7f834ea5e
lyj00912 <3122742760@qq.com>
2022-05-08 10:59:38 +0800

    decoupled LCD_Write_AllData()

--- LCD_Show.ino
+++ LCD_Show.ino
@@ -33,11 +33,17 @@ void setup()
   LCD_Init(Lcd_ScanDir, 200);
   
   LCD_SetWindow(0, 0, LCD_WIDTH, LCD_HEIGHT);
-  LCD_Write_AllData(RGB(31, 0, 0), (uint32_t)LCD_WIDTH * (uint32_t)LCD_HEIGHT);
   
-  SerialPrintUint32((uint32_t)LCD_WIDTH);
-  SerialPrintUint32((uint32_t)LCD_HEIGHT);
-  SerialPrintUint32((uint32_t)LCD_WIDTH * (uint32_t)LCD_HEIGHT);
+  uint16_t Data = RGB(31, 0, 0);
+  uint32_t DataLen = (uint32_t)LCD_WIDTH * (uint32_t)LCD_HEIGHT;
+  uint32_t i;
+  LCD_DC_1;
+  LCD_CS_0;
+  for(i = 0; i < DataLen; i++) {
+      SPI4W_Write_Byte(Data >> 8);
+      SPI4W_Write_Byte(Data & 0XFF);
+  }
+  LCD_CS_1;
 }
 
 void loop()
